f4d822f370cec8dfaa38f336ad3e1b3e38f7020b
Stephanie Cruz <exscruzme@gmail.com>
2018-11-16 17:28:25 -0500

    Updates

---
+++ precache-manifest.e672334cc7ffb8e205371b3f611da295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/werewolf-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "104e81d646667f33dfdd",
+    "url": "/werewolf-game/static/js/runtime~main.104e81d6.js"
+  },
+  {
+    "revision": "924e38a2dc96c2ef816d",
+    "url": "/werewolf-game/static/js/main.924e38a2.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/werewolf-game/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "924e38a2dc96c2ef816d",
+    "url": "/werewolf-game/static/css/main.f8ac7638.chunk.css"
+  },
+  {
+    "revision": "97d0a12cb946e8851f6edc0ed53fad96",
+    "url": "/werewolf-game/index.html"
+  }
+];
